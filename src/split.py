e710ae74f86a7af17655fb77fb034db7b4eed631
Michael Lawrence <lawremi@gmail.com>
2025-10-13 11:59:17 -0700

    parse markdown images and links

--- src/split.py
+++ src/split.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extract import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -18,4 +19,56 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(part, TextType.TEXT))
             else:
                 new_nodes.append(TextNode(part, text_type))
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        original_text = node.text
+        images = extract_markdown_images(original_text)
+        if not images:
+            new_nodes.append(node)
+            continue
+        
+        remaining_text = original_text
+        for image_alt, image_url in images:
+            parts = remaining_text.split(f"![{image_alt}]({image_url})", 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_url))
+            remaining_text = parts[1]
+
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        original_text = node.text
+        links = extract_markdown_links(original_text)
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        remaining_text = original_text
+        for link_text, link_url in links:
+            parts = remaining_text.split(f"[{link_text}]({link_url})", 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            remaining_text = parts[1]
+        
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+            
     return new_nodes
--- src/test_split.py
+++ src/test_split.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from split import split_nodes_delimiter
+from split import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplit(unittest.TestCase):
     def test_split_nodes_bold(self):
@@ -73,3 +73,63 @@ class TestSplit(unittest.TestCase):
         nodes = []
         new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
         self.assertEqual(new_nodes, [])
+
+    def test_split_nodes_image(self):
+        nodes = [TextNode("This is ![an image](https://i.imgur.com/zjjcJKZ.png) embedded in text", TextType.TEXT)]
+        new_nodes = split_nodes_image(nodes)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[1].text, "an image")
+        self.assertEqual(new_nodes[1].url, "https://i.imgur.com/zjjcJKZ.png")
+        self.assertEqual(new_nodes[2].text, " embedded in text")
+    
+    def test_split_nodes_image_no_image(self):
+        nodes = [TextNode("This is text without an image", TextType.TEXT)]
+        new_nodes = split_nodes_image(nodes)
+        self.assertEqual(new_nodes[0].text, "This is text without an image")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+    
+    def test_split_nodes_image_multiple(self):
+        nodes = [
+            TextNode("This is ![an image](https://i.imgur.com/zjjcJKZ.png) and this is ![another image](https://i.imgur.com/sdafJS.png) embedded in text", TextType.TEXT)
+        ]
+        new_nodes = split_nodes_image(nodes)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[1].text, "an image")
+        self.assertEqual(new_nodes[1].url, "https://i.imgur.com/zjjcJKZ.png")
+        self.assertEqual(new_nodes[2].text, " and this is ")
+        self.assertEqual(new_nodes[3].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[3].text, "another image")
+        self.assertEqual(new_nodes[3].url, "https://i.imgur.com/sdafJS.png")
+        self.assertEqual(new_nodes[4].text, " embedded in text")
+    
+    def test_split_nodes_link(self):
+        nodes = [TextNode("This is [a link](https://www.google.com) embedded in text", TextType.TEXT)]
+        new_nodes = split_nodes_link(nodes)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[1].text_type, TextType.LINK)
+        self.assertEqual(new_nodes[1].text, "a link")
+        self.assertEqual(new_nodes[1].url, "https://www.google.com")
+        self.assertEqual(new_nodes[2].text, " embedded in text")
+    
+    def test_split_nodes_link_no_link(self):
+        nodes = [TextNode("This is text without a link", TextType.TEXT)]
+        new_nodes = split_nodes_link(nodes)
+        self.assertEqual(new_nodes[0].text, "This is text without a link")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+    
+    def test_split_nodes_link_multiple(self):
+        nodes = [
+            TextNode("This is [a link](https://www.google.com) and this is [another link](https://www.bing.com) embedded in text", TextType.TEXT)
+        ]
+        new_nodes = split_nodes_link(nodes)
+        self.assertEqual(new_nodes[0].text, "This is ")
+        self.assertEqual(new_nodes[1].text_type, TextType.LINK)
+        self.assertEqual(new_nodes[1].text, "a link")
+        self.assertEqual(new_nodes[1].url, "https://www.google.com")
+        self.assertEqual(new_nodes[2].text, " and this is ")
+        self.assertEqual(new_nodes[3].text_type, TextType.LINK)
+        self.assertEqual(new_nodes[3].text, "another link")
+        self.assertEqual(new_nodes[3].url, "https://www.bing.com")
+        self.assertEqual(new_nodes[4].text, " embedded in text")
