4943d5830f9865278db7d88b2e62d2d37a6471f0
Michael Lawrence <lawremi@gmail.com>
2025-10-15 10:51:51 -0700

    static/ should have been copied into docs/, not public/

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from utils import copy_recursive
 from generate import generate_pages_recursive
 
 def main():
-    copy_recursive("static", "public")
+    copy_recursive("static", "docs")
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
