52911c1af2001c8b8b1339c12172a60792c7c0bb
Michael Lawrence <lawremi@gmail.com>
2025-10-15 10:42:16 -0700

    support passing the site basepath via the command-line

--- src/generate.py
+++ src/generate.py
@@ -8,7 +8,7 @@ def extract_title(text):
     match = re.search(pattern, text, re.MULTILINE)
     return match.group(1).strip() if match else None
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print("Generating page from", from_path, "to", dest_path, "using", template_path)
     
     with open(from_path, "r") as f:
@@ -20,16 +20,19 @@ def generate_page(from_path, template_path, dest_path):
     content = markdown_to_html_node(markdown).to_html()
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content)
-    
+
+    template.replace('href="/', f'href="{basepath}')
+    template.replace('src="/', f'src="{basepath}')
+
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for root, dirs, files in os.walk(dir_path_content):
         for file in files:
             if not file.endswith(".md"):
                 continue
             from_path = os.path.join(root, file)
             dest_path = os.path.join(dest_dir_path, os.path.relpath(from_path, dir_path_content)).replace(".md", ".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
+import sys
 from utils import copy_recursive
 from generate import generate_pages_recursive
 
-
 def main():
     copy_recursive("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
